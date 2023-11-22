9333860092ae56792164e968ba5a138275fa80c1
agnkos <101097155+agnkos@users.noreply.github.com>
2023-11-22 13:01:07 +0100

    redirects

--- src/pages/AddProject.tsx
+++ src/pages/AddProject.tsx
@@ -39,6 +39,9 @@ const AddProject = () => {
     return (
         <div>
             <h1 className="text-2xl font-bold">New Project</h1>
+            <button
+                className='mt-1 px-3 py-1 bg-teal-200  hover:bg-teal-300 shadow-[3px_3px_0_0] shadow-zinc-800 hover:translate-x-0.5 hover:translate-y-0.5'
+            >cancel</button>
             <Form action="/addproject" method="post">
                 <div className="my-2">
                     <label>Project name</label>
