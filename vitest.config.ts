3f6b9aee4ffdb11d399b50ea673c6f8f6ea01889
agnkos <101097155+agnkos@users.noreply.github.com>
2023-12-29 13:59:11 +0100

    data.error repaired

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vitest/config'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/test/setup.ts',
+        css: true,
+    },
+})
+
