c3d61aca0c76a6cdb0ea88fe54e620cbf1525796
agnkos <101097155+agnkos@users.noreply.github.com>
2024-01-10 17:50:46 +0100

    DeleteModal tests

---
+++ src/components/DeleteModal.test.jsx
@@ -0,0 +1,64 @@
+import { describe, vi, it, expect } from "vitest";
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import React from "react";
+import DeleteModal from "./DeleteModal";
+
+describe('delete modal testing', async () => {
+
+    it('delete modal is shown', () => {
+        render(
+            <DeleteModal />
+        )
+        screen.debug()
+        expect(screen.getByText('Are you sure you want to delete this ?')).toBeInTheDocument()
+
+    })
+    it('delete modal props "item" is passed', () => {
+        render(
+            <DeleteModal item={'yarn'} />
+        )
+        expect(screen.getByText('Are you sure you want to delete this yarn?')).toBeInTheDocument()
+        screen.debug()
+
+
+    })
+    it('delete modal can be closed', async () => {
+        const user = userEvent.setup()
+        const mockCallback = vi.fn()
+
+        render(
+            <DeleteModal item={'yarn'} closeModal={mockCallback} />
+        )
+        const icon = screen.getByTestId('close-icon')
+        screen.debug(icon)
+        await user.click(icon)
+
+        expect(mockCallback).toHaveBeenCalledOnce()
+    })
+    it.only('delete modal can be cancel', async () => {
+        const user = userEvent.setup()
+        const mockCallback = vi.fn()
+
+        render(
+            <DeleteModal item={'yarn'} closeModal={mockCallback} />
+        )
+        const cancelBtn = screen.getByText('Cancel')
+        screen.debug(cancelBtn)
+        await user.click(cancelBtn)
+        expect(mockCallback).toHaveBeenCalledOnce()
+    })
+    it.only('delete modal - user can confirm deleting an item', async () => {
+        const user = userEvent.setup()
+        const mockCallback = vi.fn()
+
+        render(
+            <DeleteModal item={'yarn'} deleteItem={mockCallback} />
+        )
+        const deleteBtn = screen.getByText('Delete')
+        screen.debug(deleteBtn)
+        await user.click(deleteBtn)
+        expect(mockCallback).toHaveBeenCalledOnce()
+    })
+}
+)
--- src/components/DeleteModal.tsx
+++ src/components/DeleteModal.tsx
@@ -1,6 +1,6 @@
-import { useContext } from 'react';
+// import { useContext } from 'react';
 import { XCircleIcon } from '@heroicons/react/24/outline';
-import DeleteModalContext from '../context/DeleteModalContext';
+// import DeleteModalContext from '../context/DeleteModalContext';
 
 type DeleteModalProps = {
   closeModal: () => void;
@@ -8,14 +8,15 @@ type DeleteModalProps = {
   item: string;
 };
 
-const DeleteModal = ({ deleteItem, item }: DeleteModalProps) => {
-  const { deleteModalDispatch } = useContext(DeleteModalContext);
+const DeleteModal = ({ deleteItem, item, closeModal }: DeleteModalProps) => {
+  // const { deleteModalDispatch } = useContext(DeleteModalContext);
 
   return (
     <div className='fixed top-0 left-0 flex justify-center items-center w-full h-full bg-slate-400/75'>
       <div className='max-w-xs p-6 flex flex-col gap-4 bg-slate-100 shadow-[3px_3px_0_0] shadow-zinc-800 z-50'>
-        <div onClick={() => deleteModalDispatch({ type: 'HIDE' })}>
-          <XCircleIcon className='w-6 h-6 ml-auto cursor-pointer ' />
+        {/* <div onClick={() => deleteModalDispatch({ type: 'HIDE' })}> */}
+        <div onClick={closeModal}>
+          <XCircleIcon className='w-6 h-6 ml-auto cursor-pointer ' data-testid='close-icon'/>
         </div>
         <p className='text-center'>
           Are you sure you want to delete this {item}?
@@ -28,7 +29,8 @@ const DeleteModal = ({ deleteItem, item }: DeleteModalProps) => {
             Delete
           </button>
           <button
-            onClick={() => deleteModalDispatch({ type: 'HIDE' })}
+            // onClick={() => deleteModalDispatch({ type: 'HIDE' })}
+            onClick={closeModal}
             className='block px-3 py-1 bg-teal-200  hover:bg-teal-300 shadow-[3px_3px_0_0] shadow-zinc-800 hover:translate-x-0.5 hover:translate-y-0.5'
           >
             Cancel
